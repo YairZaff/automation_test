a3907ee385aecf83ca805110d7629bfce84c89a2
turbo <leetwar@gmail.com>
2020-01-10 23:42:57 +0200

    selenium test

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
+
+# -*- coding: utf-8 -*-
+
+'''
+
+chrome webdrive version 78.0.3904.105 win32
+
+https://chromedriver.storage.googleapis.com/index.html?path=78.0.3904.105/
+
+'''
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
+
 import os
 import time
 
 driver = webdriver.Chrome()
-driver.get("https://codepen.io/yairzaff/full/abzGBpb")
 
+#alert
+driver.get("https://codepen.io/yairzaff/full/abzGBpb")
+driver.switch_to.alert.accept()
+
+#prompt
+driver.get("https://codepen.io/yairzaff/full/yLyjgdg")
+driver.switch_to.alert.send_keys("abcd")
 driver.switch_to.alert.accept()
