cb340213faf2d6b6b78a532ba3027616b225fbb1
turbo <leetwar@gmail.com>
2020-01-11 11:36:56 +0200

    prompt test

--- test.py
+++ test.py
@@ -19,7 +19,12 @@ from selenium.common.exceptions  import TimeoutException
 import time
 import threading
 import datetime
+import os
 import sys
+
+from PIL import ImageGrab
+import win32gui
+
 
 
 ###############################
@@ -33,6 +38,9 @@ crusoe_hub_url = "http://IP:PORT/wd/hub"
 test_cycles_per_browser = sys.maxsize
 
 DEFAULT_DELAY = 5
+TAKE_PICTURES = True
+input_list = ["hello","שלום","1",""]
+
 
 def msg(text):
     now = datetime.datetime.now()
@@ -41,7 +49,7 @@ def msg(text):
     else:
         print("[" + now.strftime("%Y-%m-%d %H:%M:%S") + " Thread:" + str(threading.current_thread().threadID) + " User:" + threading.current_thread().userCredentials["username"] + "] " + text)
 
-input_list = ["hello","שלום","1",""]
+
 
 class Tester():
     def __init__(self, username, password):
@@ -50,13 +58,17 @@ class Tester():
         self.proxy_exclusions = crusoe_proxy_exclusions
         self.success=0
         self.fail=0
+        
         msg ("Connecting to web driver on hub: " + self.remote_hub_url)
         #webdriver = self.connect_remote_chrome_webdriver_with_proxy(self.remote_hub_url, self.proxy_host_port, self.proxy_exclusions)
         webdriver = self.connect_my_driver() # i dont have access to crusoe's proxy
         self.time_start = str(int(time.time()))
+        self.scrnshot_dir="test_"+self.time_start+"_screenshots"
         try:
             self.test_cases(webdriver,input_list)
             print("total tests: ",(self.success+self.fail), "failed: ",self.fail)
+            if(self.fail > 0 and TAKE_PICTURES):
+                print("you can view screenshots of failed tests at:  ",self.scrnshot_dir)
         finally:
             webdriver.quit()
 
@@ -107,11 +119,13 @@ class Tester():
                 print("alert text is equal to provided text.")
             if( expect_value and alert.text != text_value ):
                 print("alert text is not equal to provided text, alert text: ",alert.text)
+                self.screen_shot(driver,"alert_wrong_text")
                 sucess = False
             alert.accept()
             print("alert accepted succesfuly.")
         except TimeoutException:
             print("no alert found.")
+            self.screen_shot(driver,"no_alert")
             sucess = False
         return sucess
     
@@ -129,13 +143,37 @@ class Tester():
                 print("confirm text is equal to provided text.")
             if( expect_value and alert.text != text_value ):
                 print("confirm text is not equal to provided text, confirm text: ",alert.text)
+                self.screen_shot(driver,"confirm_wrong_text")
             if(confirm):
                 alert.accept()
+                print("confirm closed succesfuly. (using: ok)") 
             else:
                 alert.dismiss()
-            print("confirm closed succesfuly.") 
+                print("confirm closed succesfuly. (using: cancel)") 
+            
         except TimeoutException:
-            print("error: no confirm found.")
+            print("no confirm found.")
+            self.screen_shot(driver,"no_confirm")
+
+    def prompt_check(self, driver, url=None, timeout=DEFAULT_DELAY,confirm=True , text_input=None):
+        url = self.__url_check(url,driver)
+        print('='*25,"\ntesting for prompt at ",url,"\nconfirm timeout in: ",timeout," sec(s).")
+        print("input value: ",text_input)
+        try:
+            WebDriverWait(driver, timeout).until(EC.alert_is_present(),
+                                        'Timed out waiting for PA creation ' +
+                                        'confirmation popup to appear.')
+            alert = driver.switch_to.alert
+            driver.switch_to.alert.send_keys(text_input)    
+            if(confirm):
+                alert.accept()
+                print("prompt closed succesfuly. (using: ok)") 
+            else:
+                alert.dismiss()
+                print("prompt closed succesfuly. (using: cancel)") 
+        except TimeoutException:
+            print("no prompt found.")
+            self.screen_shot(driver,"no_prompt")
 
     def __url_check(self, url, driver):
         if( url == None ):#prefom check at current page or go to new url.
@@ -147,12 +185,29 @@ class Tester():
             driver.get(url)
         return url
     
+    def screen_shot(self,driver, name):
+        if not TAKE_PICTURES:
+            return
+        time.sleep(1)
+        try:
+            os.mkdir(self.scrnshot_dir)
+        finally:
+            toplist, winlist = [], []          
+            win32gui.EnumWindows(lambda hwnd,toplist : winlist.append((hwnd, win32gui.GetWindowText(hwnd))), toplist)
+            chrome = [(hwnd, title) for hwnd, title in winlist if 'chrome' in title.lower()]
+            chrome = chrome[0]
+            hwnd = chrome[0]
+            win32gui.SetForegroundWindow(hwnd)
+            bbox = win32gui.GetWindowRect(hwnd)
+            img = ImageGrab.grab(bbox)
+            img.save(self.scrnshot_dir+"/"+name+"_"+str(int(time.time()))+".jpg",format="JPEG")
+
     def test_cases(self, webdriver, input_list):
-        
         self.alert_cases(webdriver,input_list)
         self.confirm_cases(webdriver,input_list)
-
-    def alert_cases(self,webdriver,input_list):
+        self.prompt_cases(webdriver,input_list)
+    def alert_cases(self, webdriver, input_list):
+        # code:  https://codepen.io/yairzaff/pen/YzPLQrv
         msg(" --- TESTING ALERTS ---")
         for val in input_list:
             success = self.alert_check(webdriver, url="https://codepen.io/yairzaff/full/YzPLQrv?text="+val, expect_value=True, text_value=val)
@@ -162,16 +217,37 @@ class Tester():
                 self.fail+=1
 
     def confirm_cases(self, webdriver, input_list):
+        # code: https://codepen.io/yairzaff/pen/ExaLvWp
         msg(" --- TESTING CONFIRMS ---")
-        for i in range(6):
-            self.confirm_check(webdriver,url="https://codepen.io/yairzaff/full/ExaLvWp?text=choice")
-            time.sleep(i*2)
-            success = self.alert_check(webdriver,expect_value=True,text_value="ok")
+        self.confirm_check(webdriver,url="https://codepen.io/yairzaff/full/ExaLvWp?text=choice")
+        success = self.alert_check(webdriver,expect_value=True,text_value="ok") #currently checking the returned value in alert since i can't get console.log to work properly.
+        if(success):
+            self.success+=1
+        else:
+            self.fail+=1
+        self.confirm_check(webdriver,url="https://codepen.io/yairzaff/full/ExaLvWp?text=choice",confirm=False)
+        success = self.alert_check(webdriver, expect_value=True, text_value="cancel")
+        if(success):
+            self.success+=1
+        else:
+            self.fail+=1
+
+    def prompt_cases(self,webdriver,input_list):
+        msg(" --- TESTING PROMPTS ---")
+        for val in input_list:
+            self.prompt_check(webdriver, url="https://codepen.io/yairzaff/full/yLyjgdg", confirm=True, text_input=val)
+            success = self.alert_check(webdriver, expect_value=True, text_value=val)
             if(success):
                 self.success+=1
             else:
                 self.fail+=1
-
+        for i in range(30):
+            self.prompt_check(webdriver, url="https://codepen.io/yairzaff/full/yLyjgdg", confirm=True, text_input=str(i))
+            success = self.alert_check(webdriver, expect_value=True, text_value=str(i))
+            if(success):
+                self.success+=1
+            else:
+                self.fail+=1
 
 mytester = Tester("john", "doe")
 
