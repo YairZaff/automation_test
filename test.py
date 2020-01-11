264cdaef3ef0ca7e58f8921ef8ade24e5c4f1d71
turbo <leetwar@gmail.com>
2020-01-11 11:40:06 +0200

    space's + comment's

--- test.py
+++ test.py
@@ -206,6 +206,7 @@ class Tester():
         self.alert_cases(webdriver,input_list)
         self.confirm_cases(webdriver,input_list)
         self.prompt_cases(webdriver,input_list)
+
     def alert_cases(self, webdriver, input_list):
         # code:  https://codepen.io/yairzaff/pen/YzPLQrv
         msg(" --- TESTING ALERTS ---")
@@ -233,6 +234,7 @@ class Tester():
             self.fail+=1
 
     def prompt_cases(self,webdriver,input_list):
+        # code: https://codepen.io/yairzaff/pen/yLyjgdg
         msg(" --- TESTING PROMPTS ---")
         for val in input_list:
             self.prompt_check(webdriver, url="https://codepen.io/yairzaff/full/yLyjgdg", confirm=True, text_input=val)
@@ -251,91 +253,3 @@ class Tester():
 
 mytester = Tester("john", "doe")
 
-
-'''
-for entry in driver.get_log('browser'):
-    print(entry)
-'''
-
-
-
-'''
-def alert_check(url=None,timeout=3,expect_value=False,text_value=None):
-    if(url==None):#check at current or go to new url.
-        try:
-            url=driver.current_url
-        except: #cant retrive with alert open
-            url="(current url)"
-    else:
-        driver.get(url)
-        print('='*25)
-    print("testing for alert at ",url,"\nalert timeout in: ",timeout," sec(s).")
-    if(expect_value):
-        print("expected value: ",text_value)
-    try:
-        WebDriverWait(driver, timeout).until(EC.alert_is_present(),
-                                    'Timed out waiting for PA creation ' +
-                                    'confirmation popup to appear.')
-        alert = driver.switch_to.alert
-        if(expect_value and alert.text==text_value):
-            print("alert text is equal to provided text.")
-        if(expect_value and alert.text!=text_value):
-            print("alert text is not equal to provided text, alert text: ",alert.text)
-        alert.accept()
-        print("alert accepted succesfuly.")
-    except TimeoutException:
-        print("no alert found.")
-
-
-
-def confirm_check(url=None,timeout=3,confirm=True,expect_value=False,text_value=None):
-    if(url==None):#check at current or go to new url.
-        try:
-            url=driver.current_url
-        except: #cant retrive with alert open
-            url="(current url)"
-    else:
-        driver.get(url)
-        print('='*25)
-    print("testing for confirm at ",url,"\nconfirm timeout in: ",timeout," sec(s).")
-    if(expect_value):
-        print("expected value: ",text_value)
-    driver.get(url)
-    try:
-        WebDriverWait(driver, timeout).until(EC.alert_is_present(),
-                                    'Timed out waiting for PA creation ' +
-                                    'confirmation popup to appear.')
-        alert = driver.switch_to.alert
-        if(expect_value and alert.text==text_value):
-            print("confirm text is equal to provided text.")
-        if(expect_value and alert.text!=text_value):
-            print("error: confirm text is not equal to provided text, confirm text: ",alert.text)
-        if(confirm):
-            alert.accept()
-        else:
-            alert.dismiss()
-        print("alert accepted succesfuly.")
-        
-    except TimeoutException:
-        print("error: no confirm found.")
-
-
-def prompt_test(txt): 
-    driver.get("https://codepen.io/yairzaff/full/yLyjgdg")
-    driver.switch_to.alert.send_keys(txt)
-    driver.switch_to.alert.accept()
-    returned_text=driver.switch_to.alert.text
-    driver.switch_to.alert.accept()
-
-#no alert expected. source: https://codepen.io/yairzaff/pen/YzPLVBx
-alert_check("https://codepen.io/yairzaff/full/YzPLVBx")
-
-#expecting alert. source: https://codepen.io/yairzaff/pen/abzGBpb
-alert_check("https://codepen.io/yairzaff/full/abzGBpb")
-
-#expecting alert with text "hello"
-alert_check("https://codepen.io/yairzaff/full/abzGBpb",expect_value=True,text_value="hello")
-
-#expecting alert with text "שלום"
-alert_check("https://codepen.io/yairzaff/full/rNavwOx",expect_value=True,text_value="שלום")
-'''
